9165f23c9c581eed11a823812ba4c0f526172975
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2022-10-04 19:50:59 +0900

    장애물 랜덤 생성

--- Raise Band/Assets/Assets/MiniGame2/ObstacleGenerator.cs
+++ Raise Band/Assets/Assets/MiniGame2/ObstacleGenerator.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
+    public GameObject Canvas;
     public GameObject obstaclePrefeb;
-    float span = 1.0f;
+    float span = 1.5f;
     float delta = 0;
 
     void Start()
     {
-        Instantiate(obstaclePrefeb);
+        GameObject newObs = Instantiate(obstaclePrefeb, transform.position, transform.rotation) as GameObject;
+        newObs.transform.SetParent(GameObject.FindGameObjectWithTag("obs").transform, false);
     }
 
     // Update is called once per frame
@@ -21,11 +23,14 @@ public class ObstacleGenerator : MonoBehaviour
         if (this.delta > this.span)
         {
             this.delta = 0;
-            GameObject go = Instantiate(obstaclePrefeb) as GameObject;
 
-            int px = Random.Range(-6, 7);
-            go.transform.position = new Vector3(px, 7, 0);
+            int px = Random.Range(-960, 960);
+            GameObject newObs = Instantiate(obstaclePrefeb, new Vector3(px, 540, 0), transform.rotation) as GameObject;
+            newObs.transform.SetParent(GameObject.FindGameObjectWithTag("obs").transform, false);
+
+            // float px = Random.Range(-6, 7);
+            //newObs.transform.position= new Vector3(px, px, px);
         }
-        
+
     }
 }
