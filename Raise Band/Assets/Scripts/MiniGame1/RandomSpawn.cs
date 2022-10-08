21bb98f56969b4a4ce42c906584ea94480c293d5
baikdk <baikdk@g.hongik.ac.kr>
2022-10-08 13:13:05 +0900

    랜덤스포 完
    -랜덤 스폰 수정 완료

--- Raise Band/Assets/Scripts/MiniGame1/RandomSpawn.cs
+++ Raise Band/Assets/Scripts/MiniGame1/RandomSpawn.cs
@@ -7,51 +7,73 @@ public class RandomSpawn : MonoBehaviour
     [SerializeField] // private 함수를 inspector에서 관리 가능하게 함
     GameObject A, B, C, D, E, F;
 
-    int SpawnObj;
+    Vector2 creatPoint;
+    int[] SpawnObj = new int[6];
 
+    void Start()
+    {
+        Spawn();
+    }
     void Update()
     {
         SpawnPlay();
     }
 
-    void SpawnPlay()
+    void Spawn()
     {
-        bool keydown = Input.GetKeyDown(KeyCode.Space);
+        creatPoint = transform.position;
+        creatPoint.x = 231f;
+        creatPoint.y = 419f;
+        transform.position = creatPoint;
 
-        if (keydown)
+        for (int i = 0; i < 6; i++)
         {
-            SpawnObj = Random.Range(1, 7);
+            SpawnObj[i] = Random.Range(1, 7);
 
-            if(SpawnObj == 1)
+            if (SpawnObj[i] == 1)
             {
-                Instantiate(A, transform.position, Quaternion.identity);
+                Instantiate(A, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
 
-            else if(SpawnObj == 2)
+            else if (SpawnObj[i] == 2)
             {
-                Instantiate(B, transform.position, Quaternion.identity);
+                Instantiate(B, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
 
-            else if(SpawnObj == 3)
+            else if (SpawnObj[i] == 3)
             {
-                Instantiate(C, transform.position, Quaternion.identity);
+                Instantiate(C, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
 
-            else if(SpawnObj == 4)
+            else if (SpawnObj[i] == 4)
             {
-                Instantiate(D, transform.position, Quaternion.identity);
+                Instantiate(D, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
 
-            else if(SpawnObj == 5)
+            else if (SpawnObj[i] == 5)
             {
-                Instantiate(E, transform.position, Quaternion.identity);
+                Instantiate(E, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
 
             else
             {
-                Instantiate(F, transform.position, Quaternion.identity);
+                Instantiate(F, creatPoint, Quaternion.identity, GameObject.Find("Canvas").transform);
+                creatPoint.x += 100f;
             }
-
+        }
+    }
+    void SpawnPlay()
+    {
+        bool keydown = Input.GetKeyDown(KeyCode.Space);
+        
+        if (keydown)
+        {
+            Spawn();
         }
     }
 }
