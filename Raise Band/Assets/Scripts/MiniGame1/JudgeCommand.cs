ab4068b187869f439962a279b3dad7a8c3e6828c
groot_ljm <beckaljm@naver.com>
2022-10-10 23:43:22 +0900

    Revert "Revert "판정 구현""
    This reverts commit 66a827a056d9b93e23976e0321579d2c85f600b2.

--- Raise Band/Assets/Scripts/MiniGame1/JudgeCommand.cs
+++ Raise Band/Assets/Scripts/MiniGame1/JudgeCommand.cs
@@ -1,19 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JudgeCommand : MonoBehaviour
 {
-    public GameObject RandomSpawn;
-    // Start is called before the first frame update
-    void Start()
+    static int i = 0;
+
+    public void ButtonA()
     {
-        
+        while(i <= 5)
+        {
+            if (GameObject.FindWithTag("TagA")==gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonB()
     {
-        
+        while (i <= 5)
+        {
+            if (GameObject.FindWithTag("TagB") == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
+    }
+
+    public void ButtonC()
+    {
+        while (i <= 5)
+        {
+            if (GameObject.FindWithTag("TagC") == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
+    }
+
+    public void ButtonD()
+    {
+        while (i <= 5)
+        {
+            if (GameObject.FindWithTag("TagD") == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
+    }
+
+    public void ButtonE()
+    {
+        while (i <= 5)
+        {
+            if (GameObject.FindWithTag("TagE") == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
+    }
+
+    public void ButtonF()
+    {
+        while (i <= 5)
+        {
+            if (GameObject.FindWithTag("TagF") == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
+            {
+                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
+                i++;
+            }
+        }
     }
 }
